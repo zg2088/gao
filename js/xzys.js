6f361ecc64924f41e0152cd1bd4dedc02613cffe
gitbox520 <137974063+gitbox520@users.noreply.github.com>
2024-03-07 21:27:46 +0800

    Update xzys.js

--- js/xzys.js
+++ js/xzys.js
@@ -37,105 +37,55 @@ var rule = {
 		tabs:`js:
 pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
 TABS=[]
-let d = pdfa(html, 'div.container div.row a');
-let tabsa = [];
-let tabsq = [];
-let tabsm = false;
-let tabse = false;
+let d = pdfa(html, 'div.tc-box a');
+let addtabs = false;
 d.forEach(function(it) {
-	let burl = pdfh(it, 'a&&href');
-	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
-		tabsa.push("阿里雲盤");
-	}else if (burl.startsWith("https://pan.quark.cn/s/")){
-		tabsq.push("夸克網盤");
+	let burl = pdfh(it, 'a&&href') || '';
+
+	if (burl.startsWith("https://www.aliyundrive.com/s/") ||
+	 burl.startsWith("https://www.alipan.com/s/") || 
+	 burl.startsWith("https://pan.quark.cn/s/") ||
+	 burl.startsWith("https://pan.xunlei.com/s/") ||
+	 burl.startsWith("https://pan.baidu.com/s/") ){
+		addtabs = true;
 	}else if (burl.startsWith("magnet")){
-		tabsm = true;
+		addtabs = true;
 	}else if (burl.startsWith("ed2k")){
-		tabse = true;
+		addtabs = true;
 	}
 });
-if (tabsm === true){
-	TABS.push("磁力");
+if (addtabs === true){
+	TABS.push("分享链接");
 }
-if (tabse === true){
-	TABS.push("電驢");
-}
-if (false && tabsa.length + tabsq.length > 1){
-	TABS.push("選擇右側綫路");
-}
-let tmpIndex;
-tmpIndex=1;
-tabsa.forEach(function(it){
-	TABS.push(it + tmpIndex);
-	tmpIndex = tmpIndex + 1;
-});
-tmpIndex=1;
-tabsq.forEach(function(it){
-	TABS.push(it + tmpIndex);
-	tmpIndex = tmpIndex + 1;
-});
 log('xzys TABS >>>>>>>>>>>>>>>>>>' + TABS);
 `,
 		lists:`js:
 log(TABS);
 pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
 LISTS = [];
-let d = pdfa(html, 'div.container div.row a');
-let lista = [];
-let listq = [];
-let listm = [];
-let liste = [];
+let d = pdfa(html, 'div.tc-box a');
+let listurl = [];
 d.forEach(function(it){
-	let burl = pdfh(it, 'a&&href');
-	let title = pdfh(it, 'a&&Text');
+	let burl = pdfh(it, 'a&&href') || '';
+	let title = pdfh(it, 'a&&Text') || '';
 	log('dygang title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
 	log('dygang burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
 	let loopresult = title + '$' + burl;
-	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
-		if (true){
-		if (TABS.length==1){
-			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
-		}else{
-			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
-		}
-		}else{
-			burl = "push://" + burl;
-		}
-		loopresult = title + '$' + burl;
-		lista.push(loopresult);
-	}else if (burl.startsWith("https://pan.quark.cn/s/")){
-		if (true){
-		if (TABS.length==1){
-			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
-		}else{
-			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
-		}
-		}else{
-                        burl = "push://" + burl;
-                }
-		loopresult = title + '$' + burl;
-		listq.push(loopresult);
+	if (burl.startsWith("https://www.aliyundrive.com/s/") ||
+	 burl.startsWith("https://www.alipan.com/s/") || 
+	 burl.startsWith("https://pan.quark.cn/s/") ||
+	 burl.startsWith("https://pan.xunlei.com/s/") ||
+	 burl.startsWith("https://pan.baidu.com/s/") ){
+		listurl.push(loopresult);
 	}else if (burl.startsWith("magnet")){
-		listm.push(loopresult);
+		listurl.push("magnet$" + loopresult);
 	}else if (burl.startsWith("ed2k")){
-		liste.push(loopresult);
+		listurl.push("magnet$" + loopresult);
 	}
 });
-if (listm.length>0){
-	LISTS.push(listm);
+if (listurl.length>0){
+	LISTS.push(listurl);
 }
-if (liste.length>0){
-	LISTS.push(liste);
-}
-if (false && lista.length + listq.length > 1){
-	LISTS.push(["選擇右側綫路，或3秒後自動跳過$http://127.0.0.1:10079/delay/"]);
-}
-lista.forEach(function(it){
-	LISTS.push([it]);
-});
-listq.forEach(function(it){
-	LISTS.push([it]);
-});
 `,
 
 	},
